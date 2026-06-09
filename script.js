f6710a50b213e9abe753c115162d10983d12f70b
Miguel Denisovas <baiozin.denisovas@gmail.com>
2026-06-09 20:23:53 -0300

    mudanças no html e no estilo

--- script.js
+++ script.js
@@ -1,14 +1,69 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-  $("#toggleConteudo").click(function(){
+  $("#toggleConteudo").click(function () {
+    $("#conteudoExtra").slideToggle();
+  });
 
-      $("#conteudoExtra").slideToggle();
+  $("#modoEscuro").click(function () {
+
+    $("body").toggleClass("dark-mode");
+
+    if ($("body").hasClass("dark-mode")) {
+      $(this).html("☀️");
+    } else {
+      $(this).html("🌙");
+    }
 
   });
 
-  $("#modoEscuro").click(function(){
+});
 
-      $("body").toggleClass("dark-mode");
+$(document).ready(function () {
+
+  $("#toggleConteudo").click(function () {
+    $("#conteudoExtra").slideToggle();
+  });
+
+  $("#modoEscuro").click(function () {
+    $("body").toggleClass("dark-mode");
+  });
+
+  // Contador automático de cursos
+  let totalCursos = $("#cursos .card").length;
+  $("#contadorCursos").text(totalCursos);
+
+});
+
+$(document).ready(function () {
+
+  $("#toggleConteudo").click(function () {
+    $("#conteudoExtra").slideToggle();
+  });
+
+  $("#modoEscuro").click(function () {
+    $("body").toggleClass("dark-mode");
+  });
+
+  // Contador automático
+  let totalCursos = $("#cursos .curso-card").length;
+  $("#contadorCursos").text(totalCursos);
+
+  // Pesquisa de cursos
+  $("#pesquisaCurso").keyup(function () {
+
+    let texto = $(this).val().toLowerCase();
+
+    $(".curso-card").each(function () {
+
+      let titulo = $(this).find("h5").text().toLowerCase();
+
+      if (titulo.includes(texto)) {
+        $(this).parent().show();
+      } else {
+        $(this).parent().hide();
+      }
+
+    });
 
   });
 
